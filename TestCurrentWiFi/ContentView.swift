98d92afeda7b47d609b903361d53cfe0b57c4a14
Marco Wenzel <marco.wenzel@easee.com>
2022-09-06 10:02:28 +0200

    Initial Commit

---
+++ TestCurrentWiFi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestCurrentWiFi
+//
+//  Created by Marco Wenzel on 06/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestCurrentWiFi/TestCurrentWiFiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCurrentWiFiApp.swift
+//  TestCurrentWiFi
+//
+//  Created by Marco Wenzel on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestCurrentWiFiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
