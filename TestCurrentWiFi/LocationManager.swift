5cf0143f4d81ec876d0c0cb4a39d8bc292ca22fc
Marco Wenzel <marco.wenzel@easee.com>
2022-09-06 14:17:34 +0200

    initial commit

---
+++ TestCurrentWiFi/LocationManager.swift
@@ -0,0 +1,79 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The LocationManager class to receive location updates for the app.
+*/
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject {
+    
+    // MARK: - Public constants
+    
+    public static let shared = LocationManager()
+    
+    // MARK: - Private variables
+    
+    private var locationManger: CLLocationManager?
+    
+    // MARK: - Public CoreLocation Methods
+    
+    func startLocationManager() {
+        
+        if let locManager = locationManger {
+            // If you’re starting locationManager for the second time, such as in ConfirmNetwork, don't set accuracy and delegate again.
+            locManager.requestWhenInUseAuthorization()
+            locManager.startUpdatingLocation()
+            return
+        }
+        
+        let manager = CLLocationManager()
+        locationManger = manager
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyKilometer
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func stopLocationManager() {
+        locationManger?.stopUpdatingLocation()
+    }
+    
+}
+
+// MARK: - CLLocationManagerDelegate Methods
+
+extension LocationManager: CLLocationManagerDelegate {
+    
+    // MARK: - CLLocationManagerDelegate Methods
+    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        if let lastLocation = locations.last {
+//            print("<LocationManager> lastLocation:\(lastLocation.coordinate.latitude), \(lastLocation.coordinate.longitude)")
+//        }
+//    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // Detect the CLAuthorizationStatus and enable the capture of the associated SSID.
+       let accessGranted = (status == CLAuthorizationStatus.authorizedAlways ||
+            status == CLAuthorizationStatus.authorizedWhenInUse)
+        
+        print("<LocationManager> Access Granted = \(accessGranted)")
+        NotificationCenter.default.post(name: .LocationAccessStatusChanged, object: accessGranted)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        if let error = error as? CLError, error.code == .denied {
+            print("<LocationManager> Error Denied: \(error.localizedDescription)")
+            // disable location services
+            manager.stopUpdatingLocation()
+        }
+    }
+}
+
+extension Notification.Name {
+    static let LocationAccessStatusChanged = Notification.Name("LocationAccessStatusChanged")
+}
+
--- TestCurrentWiFi/TestCurrentWiFiApp.swift
+++ TestCurrentWiFi/TestCurrentWiFiApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import NetworkExtension
+import SystemConfiguration.CaptiveNetwork
 
 @main
 struct TestCurrentWiFiApp: App {
@@ -13,5 +15,78 @@ struct TestCurrentWiFiApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+struct ContentView: View {
+    @State var statusText = "You must allow location access to get WiFi info"
+    
+    var body: some View {
+        VStack {
+            Text(statusText)
+            Button("Get current network") {
+                getCurrentNetwork()
+            }
+            .buttonStyle(.bordered)
+            .padding()
+        }
+        .task {
+            // ask for location permission
+            LocationManager.shared.startLocationManager()
+        }
+        // respond to location permission status changes
+        .onReceive(NotificationCenter.default.publisher(for: .LocationAccessStatusChanged)) { notification in
+            if let granted = notification.object as? Bool {
+                self.changeStatusText(accesGranted: granted)
+            }
+        }
+    }
+    
+    func getCurrentNetwork() {
+        
+        // You must have the following to get the current wifi
+        // - com.apple.developer.networking.wifi-info entitlement is requrired from iOS 12
+        // - granted permission to user location
+        
+        if #available(iOS 14.0, *) {
+            // MARK: new API NEHotspotnetwork
+            NEHotspotNetwork.fetchCurrent { network in
+                guard let network = network else {
+                    return
+                }
+                let description = "SSID \(network.ssid) BSSID \(network.bssid)"
+                print(description)
+                
+                DispatchQueue.main.async {
+                    self.statusText = description
+                }
+            }
+        
+        } else {
+            // MARK: old API CNCopyCurrentNetworkInfo for iOS 11 to 13
+            guard let interfaceNames = CNCopySupportedInterfaces() as? [String] else {
+                return
+            }
+            
+            for interfaceName in interfaceNames {
+                guard let info = CNCopyCurrentNetworkInfo(interfaceName as CFString) as? [String: AnyObject] else {
+                    continue
+                }
+                
+                guard let ssid = info[kCNNetworkInfoKeySSID as String] as? String else {
+                    continue
+                }
+                let description = "SSID \(ssid)"
+                print(description)
+                
+                self.statusText = description
+                break
+            }
+        }
+    }
+    
+    func changeStatusText(accesGranted: Bool) {
+        statusText = accesGranted ? "✅ Location Access granted, network info available"
+                                    : "❌ Location Access denied. Go to device settings and enable location for this app to get network info"
     }
 }
